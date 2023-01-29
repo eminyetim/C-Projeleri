48b7005557105d9a176a92d654018c9036770e0f
Emin YTM <eminyetim25@gmail.com>
2023-01-30 01:15:18 +0300

    SliceOfBaklava

---
+++ CircleDraw/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CircleDraw
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Lütfen Yariçap giriniz :");
+            int r = int.Parse(Console.ReadLine());
+            
+
+            for(int i = 0 ; i < r ; i++)
+            {
+                for(int k = 0 ; k < r-i ; k++)
+                {
+                    System.Console.Write(" ");
+                }
+                System.Console.Write("*");
+
+                for(int j = 0 ; j < 2*i ; j++)
+                {
+                    System.Console.Write(" ");
+                }
+                System.Console.WriteLine("*");
+            }
+
+               for(int i = r ; i > 0 ; i--)
+            {
+                for(int k = 0 ; k < r-i ; k++)
+                {
+                    System.Console.Write(" ");
+                }
+                System.Console.Write("*");
+
+                for(int j = 0 ; j < 2*i ; j++)
+                {
+                    System.Console.Write(" ");
+                }
+                System.Console.WriteLine("*");
+            }
+        }
+    }
+}
