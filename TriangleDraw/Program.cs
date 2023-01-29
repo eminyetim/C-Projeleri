465bb7341b2ae5f7bbe8752d92e1ea1f96ec722b
Emin YTM <eminyetim25@gmail.com>
2023-01-30 00:11:58 +0300

    TriangleHomework

---
+++ TriangleDraw/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace TriangleDraw
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Lütfen Üçgen Genişliğini Giriniz :");
+            int genislik = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Lütfen Üçgen Uzunluğunu Giriniz :");
+            int uzunluk = int.Parse(Console.ReadLine());;
+            
+            for (int i = 0; i <=(genislik/2); i++)
+            {
+                System.Console.Write(" ");
+            }
+            System.Console.WriteLine("#");
+            for(int j = 0; j < uzunluk ; j++)
+            {
+                for(int i = 0 ; i <(genislik / 2)-j ; i++ )
+                {
+                    System.Console.Write(" ");
+                }
+                
+                System.Console.Write("#");
+
+                for(int k = 0 ; k <= 2*j; k++ )
+                {
+                    System.Console.Write(" ");
+                }
+                
+                 System.Console.WriteLine("#");
+            }
+            for(int i =1  ; i <= (genislik / 2)-uzunluk ; i++)
+            {
+                System.Console.Write(" ");
+            } 
+            for (int i = 0; i <=2*(uzunluk+1); i++)
+            {
+                System.Console.Write("#");
+            }
+
+        }
+    }
+
+}
