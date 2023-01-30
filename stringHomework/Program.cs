524737d455ba28470a33ac71092c4c61bb9df29b
Emin YTM <eminyetim25@gmail.com>
2023-01-30 23:20:58 +0300

    algoritmaString

---
+++ stringHomework/Program.cs
@@ -0,0 +1,31 @@
+﻿using System; 
+using System.Text.RegularExpressions;
+
+namespace stringHomework
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string girilenIfade = Convert.ToString(Console.ReadLine());
+
+            string[] kelimeler = girilenIfade.Split(',');
+          
+            foreach (var item in kelimeler)
+            {
+                foreach(var item2 in item)
+                {
+                    int s;
+                    if(int.TryParse(Convert.ToString(item2),out s))
+                    {
+                         continue;
+                    }
+                    System.Console.Write(item2);
+                    
+                }
+               
+               
+            }
+        }
+    }
+}
