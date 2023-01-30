a6cd6916ee512f417a15fed1048507e0b339889e
Emin YTM <eminyetim25@gmail.com>
2023-01-30 23:45:22 +0300

    string_reverse

---
+++ karakteriTerstenYazdirma/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace karakteriTerstenYazdirma
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Lütfen ifadeyi giriniz :");
+            string ifade = Convert.ToString(Console.ReadLine());
+            string[] ifadeTemp = ifade.Split(' ');
+            int size = ifadeTemp.Count();
+
+            for (int i = 0; i < 3; i++)
+            {
+                for(int j = ifadeTemp[i].Length - 1 ; j >= 0 ; j--)
+                {
+                    System.Console.Write(ifadeTemp[i][j]);
+                }
+                System.Console.Write(" ");
+            }
+        }
+    }
+}
