ea2ce5735a6a858da5dbb3d3440cded5e028915d
Emin YTM <eminyetim25@gmail.com>
2023-01-31 21:44:41 +0300

    AlanHesaplama

---
+++ AlanHesapla/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AlanHesapla
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            System.Console.WriteLine("(1) Kare");
+            System.Console.WriteLine("(2) Dikdörtgen");
+            System.Console.WriteLine("(3) Üçgen");
+            System.Console.WriteLine("(4) Daire");  
+            System.Console.WriteLine("Lütfen Şekil Seçiniz");
+            System.Console.WriteLine("(0) Çikiş");
+
+            int girilenIslem = int.Parse(Console.ReadLine());
+
+            switch (girilenIslem)
+            {
+                
+                case 1 : Sekil.Kare(); break;
+                case 2 : Sekil.Dikdortgen();break;
+                case 3 : Sekil.Ucgen(); break;
+                case 4 : Sekil.Daire();break;
+                case 0 : Environment.Exit(0); break;
+
+                default: System.Console.WriteLine("Lütfen 1 ile 4 arasi değer giriniz."); break;
+            }
+            
+        }
+    }
+}
---
+++ AlanHesapla/Sekil.cs
@@ -0,0 +1,67 @@
+using System;
+
+namespace AlanHesapla
+{
+    class Sekil
+    {
+        public static void Dikdortgen()
+        {
+            System.Console.WriteLine("Lütfen Uzun Kenari Giriniz:");
+            int kenarUzun = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Lütfen Kisa Kenari Giriniz :");
+            int kisaKenar = int.Parse(Console.ReadLine());
+
+            int alan = kisaKenar * kenarUzun;
+            int cevre = (kisaKenar * 2 )  + (kenarUzun * 2);
+
+            System.Console.WriteLine("Alan : " + alan);
+            System.Console.WriteLine("Çevre :" + cevre);
+        }
+
+        public static void Kare()
+        {
+            
+            System.Console.WriteLine("Lütfen Kenari Giriniz:");
+            int kenarUzun = int.Parse(Console.ReadLine());
+            int kisaKenar = kenarUzun;
+
+            int alan = kisaKenar * kenarUzun;
+            int cevre = (kisaKenar * 2 )  + (kenarUzun * 2);
+
+            System.Console.WriteLine("Alan : " + alan);
+            System.Console.WriteLine("Çevre :" + cevre);
+        }
+
+        public static void Ucgen()
+        {
+            
+            System.Console.WriteLine("Lütfen birinci ikinci ve üçüncü kenarı giriniz :");
+            int kenarBir = int.Parse(Console.ReadLine());
+            int kenariki = int.Parse(Console.ReadLine());
+            int kenarUc = int.Parse(Console.ReadLine());
+            int cevre = kenarBir + kenariki + kenarUc;
+            int s = cevre / 2;
+
+            int alanKaresi = s * (s - kenarBir) * (s-kenariki)*(s-kenarUc);
+            double alan = Math.Sqrt(alanKaresi);
+
+            System.Console.WriteLine("Alan : " + alan);
+            System.Console.WriteLine("Çevre :" + cevre);
+        }
+
+        public static void Daire()
+        {
+            double pi = 3.14;
+            System.Console.WriteLine("Lütfen Yaricapi Giriniz");
+            int yariCap = int.Parse(Console.ReadLine());
+
+            double alan = pi * yariCap * yariCap;
+            double cevre = 2 * pi * yariCap;
+
+            System.Console.WriteLine(alan);
+            System.Console.WriteLine(cevre);
+        }
+
+    }
+}
