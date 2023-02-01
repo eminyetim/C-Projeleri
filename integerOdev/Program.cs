328d8a9d55ac0c7af519a9d07046f79efc7db45a
Emin YTM <eminyetim25@gmail.com>
2023-02-01 18:35:05 +0300

    integerHomework

---
+++ integerOdev/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace integerOdev
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int adet = 0;
+            do
+            {
+                System.Console.WriteLine("Kaç tana sayi giriceksiniz lütfen çift olsun");
+                adet = int.Parse(Console.ReadLine());
+            }while(adet % 2 != 0);
+
+            int[] arr = new int[adet];
+            for (int i = 0; i < adet; i++)
+            {
+                System.Console.WriteLine("Lütfen " + (i+1) + ". değeri giriniz :");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            System.Console.WriteLine();
+            foreach (var item in arr)
+            {
+                System.Console.Write(item + " ");
+            }
+            System.Console.WriteLine();
+            for(int j = 0 ; j < adet ; j+=2)
+            {
+                if(arr[j] == arr[j+1])
+                {
+                    int temp = arr[j] * arr[j+1];
+                    System.Console.Write(Math.Pow(temp,2) + " ");
+                }
+                else
+                    System.Console.Write(arr[j] + arr[j+1] +  " ");
+
+            }
+
+        }
+    }
+}
