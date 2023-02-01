d29d8765c60314072613e10ea262f51ba491b140
Emin YTM <eminyetim25@gmail.com>
2023-02-02 00:00:25 +0300

    KarakterDeğiştirme

---
+++ KarakterDegistirme/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace KarakterDegistirme
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Lütfen String ifadeyi giriniz :");
+            string ifade = Convert.ToString(Console.ReadLine());
+
+            string[] arrIfade = ifade.Split(' ');
+
+            for (int i = 0; i < arrIfade.Count(); i++)
+            {
+                string tempArr =Convert.ToString(arrIfade[i]);
+                string ilk = Convert.ToString(tempArr[0]);
+                string son = Convert.ToString(tempArr[(tempArr.Length - 1)]);
+
+                
+                char[] arr = tempArr.ToCharArray();                
+                arr[0] = Convert.ToChar(son);
+                arr[tempArr.Length-1] = Convert.ToChar(ilk);
+                System.Console.WriteLine(arr);
+               
+            }
+            
+        }
+    }
+}
