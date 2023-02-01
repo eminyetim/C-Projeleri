60b873ada639ff6a7092ca45624d1825acb80103
Emin YTM <eminyetim25@gmail.com>
2023-02-01 19:42:05 +0300

    mutlakKareAlma

---
+++ mutlakKareAlma/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace mutlakKareAlma
+{
+    class Program
+    {
+        public static void Main()
+        {
+            System.Console.WriteLine("Lütfen Değer Giriniz :");
+            int adet = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[adet];
+            for (int i = 0; i < adet; i++)
+            {
+                System.Console.WriteLine("Lüfen" + (i+1) + ". değeri giriniz :");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            int farkK = 0;
+            double farkB = 0;
+            for (int i = 0; i < adet; i++)
+            {
+                if(arr[i] < 67)
+                {
+                    farkK+= (67 - arr[i]);
+                }
+                else
+                {
+                    int temp = 0;
+                    temp += -(67 - arr[i]);
+                    farkB += Math.Pow(temp , 2); 
+                }
+            }
+
+            System.Console.WriteLine(farkK + " " + farkB);
+        }
+    }
+}
