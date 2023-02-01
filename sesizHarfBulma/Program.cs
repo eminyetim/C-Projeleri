b2538082e3e0e0a9d156cff4036dbb95b5238402
Emin YTM <eminyetim25@gmail.com>
2023-02-02 01:18:05 +0300

    sesizHarfBulma

---
+++ sesizHarfBulma/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace sesizHarfBulma
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("\n\n Boşluk bırakıp kelimelerİ girin");
+
+                foreach (var item in Console.ReadLine().Split())
+                {
+                    bool def = false;
+                    for (int i = 1; i < item.Length; i++)
+                    {
+                        if (sessizMi(item[i]) == sessizMi(item[i - 1]))
+                            def = true;
+                    }
+                    Console.Write(def + " ");
+                }
+
+                bool sessizMi(char c)
+                {
+                    return !"aeıioöuüAEIİOÖUÜ".ToCharArray().Contains(c);
+                }
+            }
+        }
+    }
+}
